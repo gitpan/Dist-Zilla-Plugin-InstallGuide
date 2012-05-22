ee13a770311689eed6785b529ec0a673e1738ad4
Mike Doherty <doherty@cpan.org>
2012-05-22 04:43:01 +0000

    Import of DOHERTY/Dist-Zilla-Plugin-InstallGuide-1.200000 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-InstallGuide
    gitpan-cpan-version:      1.200000
    gitpan-cpan-path:         DOHERTY/Dist-Zilla-Plugin-InstallGuide-1.200000.tar.gz
    gitpan-cpan-author:       DOHERTY
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/Plugin/InstallGuide.pm
+++ lib/Dist/Zilla/Plugin/InstallGuide.pm
@@ -3,15 +3,14 @@ use strict;
 use warnings;
 
 package Dist::Zilla::Plugin::InstallGuide;
-BEGIN {
-  $Dist::Zilla::Plugin::InstallGuide::VERSION = '1.101461';
-}
 
 # ABSTRACT: Build an INSTALL file
+our $VERSION = '1.200000'; # VERSION
 use Moose;
 use Moose::Autobox;
 with 'Dist::Zilla::Role::InstallTool';
 with 'Dist::Zilla::Role::TextTemplate';
+
 
 my $template = q|
 This is the Perl distribution {{ $dist->name }}.
@@ -77,16 +76,16 @@ If you are installing into a system-wide directory, you may need to run:
     % sudo ./Build install
 |;
 
+
 sub setup_installer {
     my $self = shift;
-    my $manual_installation;
+    my $manual_installation = '';
     for (@{ $self->zilla->files }) {
         if ($_->name eq 'Makefile.PL') {
-            $manual_installation = $makemaker_manual_installation;
-            last;
-        } elsif ($_->name eq 'Build.PL') {
-            $manual_installation = $module_build_manual_installation;
-            last;
+            $manual_installation .= $makemaker_manual_installation;
+        }
+        elsif ($_->name eq 'Build.PL') {
+            $manual_installation .= $module_build_manual_installation;
         }
     }
     unless (defined $manual_installation) {
@@ -113,9 +112,10 @@ __PACKAGE__->meta->make_immutable;
 no Moose;
 1;
 
-
 __END__
 =pod
+
+=encoding utf-8
 
 =head1 NAME
 
@@ -123,7 +123,7 @@ Dist::Zilla::Plugin::InstallGuide - Build an INSTALL file
 
 =head1 VERSION
 
-version 1.101461
+version 1.200000
 
 =head1 SYNOPSIS
 
@@ -141,7 +141,7 @@ C<[MakeMaker]> or C<[ModuleBuild]> so that it can determine what kind of
 distribution you are building and which installation instructions are
 appropriate.
 
-=head1 FUNCTIONS
+=head1 METHODS
 
 =head2 setup_installer
 
@@ -150,36 +150,39 @@ Builds and writes the C<INSTALL> file.
 =for test_synopsis 1;
 __END__
 
-=head1 INSTALLATION
-
-See perlmodinstall for information and options on installing Perl modules.
-
-=head1 BUGS AND LIMITATIONS
-
-No bugs have been reported.
-
-Please report any bugs or feature requests through the web interface at
-L<http://rt.cpan.org/Public/Dist/Display.html?Name=Dist-Zilla-Plugin-InstallGuide>.
-
 =head1 AVAILABILITY
 
 The latest version of this module is available from the Comprehensive Perl
 Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
-site near you, or see
-L<http://search.cpan.org/dist/Dist-Zilla-Plugin-InstallGuide/>.
+site near you, or see L<https://metacpan.org/module/Dist::Zilla::Plugin::InstallGuide/>.
 
-The development version lives at
-L<http://github.com/hanekomu/Dist-Zilla-Plugin-InstallGuide/>.
-Instead of sending patches, please fork this project using the standard git
-and github infrastructure.
+=head1 SOURCE
 
-=head1 AUTHOR
+The development version is on github at L<http://github.com/doherty/Dist-Zilla-Plugin-InstallGuide>
+and may be cloned from L<git://github.com/doherty/Dist-Zilla-Plugin-InstallGuide.git>
 
-  Marcel Gruenauer <marcel@cpan.org>
+=head1 BUGS AND LIMITATIONS
+
+You can make new bug reports, and view existing ones, through the
+web interface at L<https://github.com/doherty/Dist-Zilla-Plugin-InstallGuide/issues>.
+
+=head1 AUTHORS
+
+=over 4
+
+=item *
+
+Marcel Grünauer <marcel@cpan.org>
+
+=item *
+
+Mike Doherty <doherty@cpan.org>
+
+=back
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2010 by Marcel Gruenauer.
+This software is copyright (c) 2010 by Marcel Grünauer <marcel@cpan.org>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
